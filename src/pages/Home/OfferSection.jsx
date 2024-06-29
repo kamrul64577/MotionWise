95d400fe10ad4fa2ae0b4ff6e3dcc714a3dfdaef
kamrul64577 <kamrulneubcse@gmail.com>
2024-06-29 17:47:55 +0100

    update offer section

--- src/pages/Home/OfferSection.jsx
+++ src/pages/Home/OfferSection.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const OfferSection = () => {
   return (
     <div className="flex flex-col items-center bg-custom-gradient text-white p-8 md:p-16">
-      <div className="text-left md:text-center max-w-4xl">
-        <h1 className="text-3xl md:text-4xl  mb-16 mt-6 text-transform: capitalize">
-        We guarantee 10-30+ new qualified calls every month or we work for free until you get that, & (you don't pay)
-        </h1>
-        <p className="text-xl md:text-2xl mb-8">
+      <div className="text-left md:text-center max-w-5xl">
+        <h1 className="text-3xl md:text-4xl  mb-8 mt-6 text-transform: capitalize">
+        We <span style={{ color: '#4e85de' }} className='underline'>guarantee</span> 10-30+ new qualified calls every month or we work for free until you get that, & 
+        <span style={{ color: '#4e85de' }}> (you don't pay)</span> </h1>
+        <p className="text-lg md:text-xl mb-8">
           We generate you more leads & book you more appointments organically & through cold outreach, helping you to achieve a brand and make more money.
         </p>
         <Link to='appt'>
