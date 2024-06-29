e801fee50d9dd49590e84db375688500d59eda6b
kamrul64577 <kamrulneubcse@gmail.com>
2024-06-29 16:56:17 +0100

    update offer

--- src/pages/Home/OfferSection.jsx
+++ src/pages/Home/OfferSection.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const OfferSection = () => {
   return (
-    <div className="flex flex-col items-center  min-h-screen bg-custom-gradient text-white p-8 md:p-16">
-      <div className="text-center max-w-4xl">
+    <div className="flex flex-col items-center min-h-screen bg-custom-gradient text-white p-8 md:p-16">
+      <div className="text-left md:text-center max-w-4xl">
         <h1 className="text-2xl md:text-4xl font-bold mb-20 mt-6">
-          We will book you (10-30)+ qualified appointments within 35 days. <span className='text-lime-200'> Or else you don’t pay & we will work</span> with you until we do.
+          We will book you 10 - 30+ qualified appointments Or you don’t pay & we will work with you for free until we do.
         </h1>
         <p className="text-xl md:text-2xl mb-8">
-          Unlock the potential of your business with our proven methods. We'll help you secure (10-30)+ qualified appointments within 35 days, or you don't pay. Plus, we'll continue working with you until we achieve your goals.
+          We generate you more leads & book you more appointments organically & through cold outreach, helping you to achieve a brand and make more money.
         </p>
         <Link to='appt'>
           <button className="btn btn-primary btn-lg px-8 py-4 bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 border-none text-white font-semibold rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">
--- src/pages/TimelineSection/TimelineSection.jsx
+++ src/pages/TimelineSection/TimelineSection.jsx
@@ -44,7 +44,7 @@ const TimelineSection = () => {
   return (
     <div className="bg-custom-gradient text-white py-16">
       <div className="container mx-auto px-4">
-        <div className="max-w-8xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           <div className="flex flex-col md:flex-row items-center">
             <div className="md:w-1/3 pr-12">
               <h3 className="text-6xl font-bold mb-2">Here's How It Works</h3>
