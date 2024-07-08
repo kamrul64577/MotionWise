fb58be220709e6a809e5f894711ebe3810011c25
kamrul64577 <kamrulneubcse@gmail.com>
2024-07-08 17:06:38 +0100

    upate

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 const Navbar = ({ handleScroll, offerRef, videoRef, howitworksRef }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <div className="navbar bg-custom-gradient text-white px-4">
             <div className="navbar-start lg:flex-1">
@@ -14,21 +24,23 @@ const Navbar = ({ handleScroll, offerRef, videoRef, howitworksRef }) => {
                     <li><a className="text-lg px-3 py-2" onClick={() => handleScroll(howitworksRef)}>How It Works</a></li>
                 </ul>
             </div>
-            <div className="navbar-end flex lg:hidden">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost">
+            <div className="navbar-end lg:hidden">
+                <div className="relative">
+                    <div tabIndex={0} role="button" className="btn btn-ghost" onClick={toggleMenu}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-custom-gradient rounded-box w-52">
-                        <li><Link className="text-lg" to='/'>Home</Link></li>
-                        <li><a className="text-lg" onClick={() => handleScroll(howitworksRef)}>How It Works</a></li>
-                    </ul>
+                    {isMenuOpen && (
+                        <ul className="dropdown-content mt-2 z-[1] p-2 shadow bg-custom-gradient rounded-box">
+                            <li><Link className="text-lg block py-2" to='/' onClick={closeMenu}>Home</Link></li>
+                            <li><a className="text-lg block py-2" onClick={() => { handleScroll(howitworksRef); closeMenu(); }}>How It Works</a></li>
+                        </ul>
+                    )}
                 </div>
             </div>
             <div className="navbar-end hidden md:flex">
-                <Link to='appt' className="btn btn-primary btn-lg px-8 py-4 bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 border-none text-white font-semibold rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hidden md:inline-block">Call Now</Link>
+                <Link to='appt' className="btn btn-primary btn-lg px-8 py-4 bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 border-none text-white font-semibold rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">Call Now</Link>
             </div>
         </div>
     );
