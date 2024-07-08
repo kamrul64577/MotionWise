4db88277cbbbdbf4e1e11c808c7a5823f588f36b
kamrul64577 <kamrulneubcse@gmail.com>
2024-07-08 16:55:27 +0100

    update menubar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -5,9 +5,18 @@ import logo from '../../assets/logo.png';
 const Navbar = ({ handleScroll, offerRef, videoRef, howitworksRef }) => {
     return (
         <div className="navbar bg-custom-gradient text-white px-4">
-            <div className="navbar-start">
+            <div className="navbar-start lg:flex-1">
+                <img className="w-42 h-16" src={logo} alt="logo" />
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link className="text-lg px-3 py-2" to='/'>Home</Link></li>
+                    <li><a className="text-lg px-3 py-2" onClick={() => handleScroll(howitworksRef)}>How It Works</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end flex lg:hidden">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div tabIndex={0} role="button" className="btn btn-ghost">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
@@ -17,15 +26,8 @@ const Navbar = ({ handleScroll, offerRef, videoRef, howitworksRef }) => {
                         <li><a className="text-lg" onClick={() => handleScroll(howitworksRef)}>How It Works</a></li>
                     </ul>
                 </div>
-                <img className='w-42 h-16' src={logo} alt="logo" />
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link className="text-lg px-3 py-2" to='/'>Home</Link></li>
-                    <li><a className="text-lg px-3 py-2" onClick={() => handleScroll(howitworksRef)}>How It Works</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
+            <div className="navbar-end hidden md:flex">
                 <Link to='appt' className="btn btn-primary btn-lg px-8 py-4 bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 border-none text-white font-semibold rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hidden md:inline-block">Call Now</Link>
             </div>
         </div>
