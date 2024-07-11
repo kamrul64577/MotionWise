f2778fe697929fcd6e5b36ae860feac323e2729c
kamrul64577 <kamrulneubcse@gmail.com>
2024-07-11 20:18:24 +0100

    pdate sls

--- src/pages/Home/Video.jsx
+++ src/pages/Home/Video.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Video = () => {
-
     return (
         <div className="bg-gray-100 py-10 flex justify-center items-center bg-custom-gradient text-white">
             <div className="w-full max-w-5xl px-4">
-                <div className="relative w-full h-0 pb-[56.25%]"> {/* 16:9 aspect ratio */}
+                <div style={{ position: 'relative', paddingBottom: '64.5933014354067%', height: '0' }}>
                     <iframe
-                        className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
-                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?modestbranding=1&rel=0&showinfo=0"
-                        title="YouTube video"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        src="https://www.loom.com/embed/e340ed746bda488caf5b531d41ba0ff0?autoplay=1&sid=43092e32-c76e-44a4-89da-d569a5c2b58a"
+                        frameBorder="0"
+                        webkitAllowFullScreen
+                        mozAllowFullScreen
                         allowFullScreen
+                        style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
                     ></iframe>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Video
+export default Video;
