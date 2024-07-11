33b3be3f0434d7962dddde1dc1271ec00054190b
kamrul64577 <kamrulneubcse@gmail.com>
2024-07-11 21:45:32 +0100

    vdo update

--- src/pages/Home/Video.jsx
+++ src/pages/Home/Video.jsx
@@ -4,15 +4,19 @@ const Video = () => {
     return (
         <div className="bg-gray-100 py-10 flex justify-center items-center bg-custom-gradient text-white">
             <div className="w-full max-w-5xl px-4">
-                <div style={{ position: 'relative', paddingBottom: '64.5933014354067%', height: '0' }}>
-                    <iframe
-                        src="https://www.loom.com/embed/e340ed746bda488caf5b531d41ba0ff0?autoplay=1&sid=43092e32-c76e-44a4-89da-d569a5c2b58a"
-                        frameBorder="0"
-                        webkitAllowFullScreen
-                        mozAllowFullScreen
-                        allowFullScreen
-                        style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
-                    ></iframe>
+                <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+                    <div className="video-wrapper" style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}>
+                        <div>
+                            <iframe
+                                src="https://www.loom.com/embed/e340ed746bda488caf5b531d41ba0ff0?sid=17b38290-90a0-4d95-95c8-8d19e0beecf9"
+                                title="Loom Video"
+                                frameBorder="0"
+                                allowFullScreen
+                                style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+                            ></iframe>
+                        </div>
+                    </div>
+                    <div className="bottom-gradient"></div>
                 </div>
             </div>
         </div>
@@ -20,3 +24,4 @@ const Video = () => {
 }
 
 export default Video;
+
