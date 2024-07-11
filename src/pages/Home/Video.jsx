c3d62c18a56a05632567a17663e2f9c3bd0fc2c3
kamrul64577 <kamrulneubcse@gmail.com>
2024-07-11 21:47:41 +0100

    vdo update

--- src/pages/Home/Video.jsx
+++ src/pages/Home/Video.jsx
@@ -10,6 +10,7 @@ const Video = () => {
                             <iframe
                                 src="https://www.loom.com/embed/e340ed746bda488caf5b531d41ba0ff0?sid=17b38290-90a0-4d95-95c8-8d19e0beecf9"
                                 title="Loom Video"
+                                allow="autoplay; encrypted-media"
                                 frameBorder="0"
                                 allowFullScreen
                                 style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
